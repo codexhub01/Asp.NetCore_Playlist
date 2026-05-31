254b214589366a8e6107b2b9e532597be86de948
codexhub01 <mayankpal4104@gmail.com>
2026-05-31 19:25:32 +0530

    Fluent API Async Validators in ASP.NET Core MVC

---
+++ Asp.NetCore_Playlist/Models/EmployeeValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Asp.NetCore_Playlist.Models
+{
+    public class EmployeeValidator :AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(s => s.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
